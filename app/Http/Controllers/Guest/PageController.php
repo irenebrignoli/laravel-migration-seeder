99e2de601f7c92116daaee2ec21b343ee985888f
Irene Brignoli <ire.brignoli@gmail.com>
2023-05-12 17:32:20 +0200

    esercizio finito con bonus

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
 
-        return view('home');
+        $dateNow = date('Y-m-d');
+
+        $trains = Train::whereDate('departure_date', '=', $dateNow)->get();
+
+        //$trains = Train::all();
+
+        //dd($trains);
+
+        return view('home',compact('trains'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,48 @@
 @section('title document', 'Trains table')
 
 @section('content')
-<h1>treni siii</h1>
-@endsection
+<div class="container">
+
+  <h1 class="text-center my-5">Train table</h1>
+
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">Company</th>
+        <th scope="col">Departure Station</th>
+        <th scope="col">Arrival Station</th>
+        <th scope="col">Departure Time</th>
+        <th scope="col">Departure Date</th>
+        <th scope="col">Arrival Time</th>
+        <th scope="col">Arrival Date</th>
+        <th scope="col">Code</th>
+        <th scope="col">Wagons</th>
+        <th scope="col">In time</th>
+        <th scope="col">Confirmed</th>
+      </tr>
+    </thead>
+    <tbody>
+      @forelse ( $trains as $train)
+        <tr>
+          <td>{{$train->company}}</td>
+          <td>{{$train->departure_station}}</td>
+          <td>{{$train->arrival_station}}</td>
+          <td>{{$train->departure_time}}</td>
+          <td>{{$train->departure_date}}</td>
+          <td>{{$train->arrival_time}}</td>
+          <td>{{$train->arrival_date}}</td>
+          <td>{{$train->code}}</td>
+          <td>{{$train->wagon}}</td>
+          <td>{{$train->in_time}}</td>
+          <td>{{$train->confirmed}}</td>
+        </tr>
+      @empty
+        <div class="alert alert-warning" role="alert">
+          No trains founded
+        </div>
+    </tbody>
+  </table>
+
+</div>
+
+@endforelse
