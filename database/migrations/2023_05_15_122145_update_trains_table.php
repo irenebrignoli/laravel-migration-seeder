224ff8d7cd239eac6acd3e2979246f4e6b080761
Irene Brignoli <ire.brignoli@gmail.com>
2023-05-15 14:48:22 +0200

    rinominate colonne

---
+++ database/migrations/2023_05_15_122145_update_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('departure_date');
+            $table->dropColumn('arrival_date');
+            $table->string('code', 5)->change();
+            $table->renameColumn('code', 'train_code');
+            $table->renameColumn('wagon', 'wagons_number');
+            $table->boolean('in_time')->change();
+            $table->boolean('confirmed')->change();
+            $table->renameColumn('in_time', 'on_time');
+            $table->renameColumn('confirmed', 'cancelled');
+    
+        });
+      
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
